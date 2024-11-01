4c038a9d2f227297946f48564bdf7472c3ad28c2
Guga Gongadze <gugongadze@gmail.com>
2024-11-01 13:45:05 +0400

    feat: header, footer and main content layout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+export function Footer() {
+  return (
+    <footer
+      style={{
+        background: "gray",
+        width: "100%",
+        height: 100,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      Footer
+    </footer>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+export function Header() {
+  return (
+    <header
+      style={{
+        background: "gray",
+        width: "100%",
+        height: 100,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      Header
+    </header>
+  );
+}
